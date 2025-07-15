6211c208cb2711f16c12671a00438af88e96effc
maxo <max.orr@mail.com>
2025-07-15 11:20:51 -0400

    build(weatherflow-collector): using common repo version and grafana

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-docker build --no-cache . -t maxo99/weatherflow-collector:latest
+#!/bin/bash
+# Read version from parent directory
+VERSION=$(grep -oP '(?<=version=).*' ../version)
+
+cp -r ../grafana/dashboards ./grafana
+cp -r ../grafana/provisioning ./grafana
+
+docker build --no-cache -t maxo99/weatherflow-collector:${VERSION} -t maxo99/weatherflow-collector:latest  .
+# docker push maxo99/weatherflow-collector:latest
